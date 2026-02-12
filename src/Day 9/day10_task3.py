54c6c7bec8e223defdf6c693100d9220ba5ed5e4
ShifaliSalian <shifaliksalian01@gmail.com>
2026-02-12 15:11:34 +0530

    Delete unused file

--- src/Day 9/day10_task3.py
+++ src/Day 9/day10_task3.py
@@ -1,8 +1,1 @@
-# Task 3
-import pandas as pd
-df = pd.read_csv("location_dirty_data.csv")
-print(df["Location"].unique())
-df["Location"] = df["Location"].str.strip()
-df["Location"] = df["Location"].str.title() 
-print("Unique location values:")
-print(df["Location"].unique())
+
